ff6950b5bed05581fe25ad1c31139f6a9b311afd
Alzan_72 <abdulazzam72@gmail.com>
2022-11-12 17:21:38 +0700

    update-2

--- Tabel-latihan/tambah-aksi-copy.php
+++ Tabel-latihan/tambah-aksi-copy.php
@@ -13,16 +13,8 @@ $wed=@$_POST['wed'];
 $thur=@$_POST['thur'];
 
 $jumlah_id=count($ID);
-$jumlah_week=count($week);
-$jumlah_year=count($year);
-$jumlah_date=count($date);
-$jumlah_month=count($month);
-$jumlah_mon=count($mon);
-$jumlah_tue=count($tue);
-$jumlah_wed=count($wed);
-$jumlah_thur=count($thur);
 
-for ($i=0; $i < $jumlah_week; $i++) { 
+for ($i=0; $i < $jumlah_id; $i++) { 
     $sql="INSERT INTO `silabus` (`ID`, `Year`, `month`, `date`, `week`, `mon`, `tue`, `wed`, `thur`) VALUES ( NULL, '$year[$i]', '$month[$i]', '$date[$i]', '$week[$i]', '$mon[$i]', '$tue[$i]', '$wed[$i]', '$thur[$i]');";
 
     $conn->query($sql);
