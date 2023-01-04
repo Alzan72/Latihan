4ee6cadf4d70cae05e9822b39937524d63822d24
Alzan_72 <abdulazzam72@gmail.com>
2023-01-04 09:35:19 +0700

    tes

--- oop.php
+++ oop.php
@@ -20,16 +20,35 @@ class barang {
      }
 }
 
+class benda {
+
+ public $nama,
+       $wujud;
+
+ public function set_nama($input){
+      return $this->nama=$input;
+ }
+ 
+
+};
+
+
 //object
 $barang1=new barang("kulkas","samsung",2000000);
 $barang2=new barang("Kipas","miyako",240000);
 $barang3=new barang("Sepatu","Nike",2400000);
 $barang4=new barang("Mobil","Alphard",24000000);
+$barang5=new benda();
+
+ 
+
+
 
 echo 'Produk yang pertama adalah '.$barang1->data().'<br>';
 echo 'Produk yang Kedua adalah '.$barang2->data().'<br>';
 echo 'Produk yang ketiga adalah '.$barang3->data().'<br>';
-echo 'Produk yang keempat adalah '.$barang4->data();
+echo 'Produk yang keempat adalah '.$barang4->data().'<br>';
+echo $barang5->set_nama('kipas');
 
 
 ?>
