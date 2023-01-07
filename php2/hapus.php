557257caaf21ee3c62f87951820e8674e164a44e
Alzan_72 <abdulazzam72@gmail.com>
2023-01-07 16:32:52 +0700

    hapus on process

--- php2/hapus.php
+++ php2/hapus.php
@@ -1,21 +1,29 @@
 <?php
 include 'koneksi.php';
 
-$hapus=$_POST['hapus'];
-// $foto=$_POST['file'];
 
+$hapus=$_POST['id'];
+$d=explode(',', $hapus[0]);
+$id_pen=$d[0];
+$id_prof=$d[1];
+
+// $foto=$_POST['file'];
 // var_dump($hapus);die;
 if (isset($hapus)) {
     // unlink('img/'.$hapus);
     $jumlah_data=count($hapus);
     for ($i=0; $i < $jumlah_data; $i++) { 
+        var_dump($id_pen[$i]);
+echo "<br> ";
+var_dump( $id_prof[$i]);die;
         $mysql="SELECT foto FROM profil WHERE ID=$hapus[$i]";
         $hasil=$conn->query($mysql);
         $data=$hasil->fetch_assoc();
         $file=@$data['foto'];
         unlink('img/'.$file);
-        
-        $sql="DELETE FROM profil JOIN penempatan on penempatan.id_profil=profil.ID JOIN kelompok on kelompok.ID=penempatan.id  WHERE `id` = $hapus[$i]";
+
+        $sql="DELETE FROM `profil` WHERE `ID` = $hapus[$i]";
+        $sql="DELETE FROM profil JOIN penempatan on penempatan.id_profil=profil.ID JOIN kelompok on kelompok.ID=penempatan.id WHERE `ID` = $hapus[$i]";
 
         $conn->query($sql);
     }
@@ -23,5 +31,4 @@ if (isset($hapus)) {
 }else {
     header ("location:tampil.php?pesan=gagal");
 }
-
 ?>
